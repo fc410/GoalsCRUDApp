95c84bb3cae21df327d4cfd122edc0a87d56428d
Fernando Crespo <54721672+fc410@users.noreply.github.com>
2022-11-07 14:34:27 -0800

    JWT
    Added JWT functionality for when the user logs in and registers in my API

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -40,7 +40,8 @@ const registerUser = asyncHandler(async (req, res) =>{
         res.status(201).json({
             id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateJWT(user._id),
         });
     } else {
         res.status(400);
@@ -61,7 +62,8 @@ const loginUser = asyncHandler(async (req, res) =>{
         res.json({
             id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateJWT(user._id),
         })
     } else {
         res.status(400);
@@ -72,10 +74,17 @@ const loginUser = asyncHandler(async (req, res) =>{
 
 // @desc    Get user data
 // @route   GET /api/users/me
-// @access  Public
+// @access  Private
 const getMe = asyncHandler(async (req, res) =>{
     res.json({message: "Display my data"})
 })
+
+// Generate JWT
+const generateJWT = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 
 module.exports = {
     registerUser,
